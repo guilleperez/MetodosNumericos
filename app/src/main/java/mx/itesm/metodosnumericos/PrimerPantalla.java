813cf26942eb6776089fa3a9f2a96c31487c2640
MauPerez <mau.m.perez@hotmail.com>
2017-04-24 11:46:07 -0500

    Me la estoy rifando putos

---
+++ app/src/main/java/mx/itesm/metodosnumericos/PrimerPantalla.java
@@ -0,0 +1,66 @@
+package mx.itesm.metodosnumericos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class PrimerPantalla extends AppCompatActivity {
+
+           protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_primer_pantalla);
+
+            Button boton = (Button)findViewById(R.id.botonUno);
+            boton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast toast =  Toast.makeText(PrimerPantalla.this,"Prueba", Toast.LENGTH_LONG);
+                    toast.show();
+                    Intent inti = new Intent(getBaseContext(), SegundaPantalla.class);
+                    startActivity(inti);
+                }
+            });
+
+            Button botonDos = (Button)findViewById(R.id.botonDos);
+            botonDos.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent inti = new Intent(getBaseContext(), SegundaPantalla.class);
+                    startActivity(inti);
+                }
+            });
+
+            Button botonTres = (Button)findViewById(R.id.botonTres);
+            botonTres.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent inti = new Intent(getBaseContext(), SegundaPantalla.class);
+                    startActivity(inti);
+                }
+            });
+
+            Button botonCuatro = (Button)findViewById(R.id.botonCuatro);
+            botonCuatro.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent inti = new Intent(getBaseContext(), SegundaPantalla.class);
+                    startActivity(inti);
+                }
+            });
+
+            Button botonPaginaDos = (Button)findViewById(R.id.paginaDos);
+            botonPaginaDos.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent inti = new Intent(getBaseContext(), TercerPantalla.class);
+                    startActivity(inti);
+                }
+            });
+
+
+
+        }
+}
--- app/src/main/java/mx/itesm/metodosnumericos/TercerPantalla.java
+++ app/src/main/java/mx/itesm/metodosnumericos/TercerPantalla.java
@@ -1,26 +1,27 @@
 package mx.itesm.metodosnumericos;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
-public class PantallaSplash extends AppCompatActivity {
+public class TercerPantalla extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_splash);
+        setContentView(R.layout.activity_tercer_pantalla);
 
-        Button boton = (Button) findViewById(R.id.inicio);
-        boton.setOnClickListener(new View.OnClickListener() {
+        ImageButton back = (ImageButton)findViewById(R.id.regresoPantallaUno);
+
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent inti = new Intent(getBaseContext(),PrimerPantalla.class);
                 startActivity(inti);
-
             }
         });
+
     }
 }
