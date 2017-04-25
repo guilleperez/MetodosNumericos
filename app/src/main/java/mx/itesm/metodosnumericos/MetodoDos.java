79eb025dc6ee3f0839eed1a3031a7300cef8cb82
MauPerez <mau.m.perez@hotmail.com>
2017-04-24 22:33:06 -0500

    Grafica de Seno rifada

--- app/src/main/java/mx/itesm/metodosnumericos/MetodoDos.java
+++ app/src/main/java/mx/itesm/metodosnumericos/MetodoDos.java
@@ -1,10 +1,19 @@
 package mx.itesm.metodosnumericos;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
 
 public class MetodoDos extends AppCompatActivity {
 
@@ -21,5 +30,43 @@ public class MetodoDos extends AppCompatActivity {
                 startActivity(inti);
             }
         });
+
+        LineChart lineChart = (LineChart)findViewById(R.id.grafica);
+
+        ArrayList<String> xs = new ArrayList<>();
+        ArrayList<Entry> ys = new ArrayList<>();
+
+
+        double x = 0;
+        int max = 500;
+        for(int i = 0; i<max ;i++){
+            float sin = Float.parseFloat(String.valueOf(Math.sin(x)));
+            x = x + 0.1;
+            ys.add(new Entry(sin,i));
+            xs.add(i,String.valueOf(x));
+        }
+        String[] xss = new String[xs.size()];
+        for(int i = 0; i<xs.size();i++){
+            xss[i] = xs.get(i).toString();
+
+        }
+
+        ArrayList<ILineDataSet> lineDataSets = new ArrayList<>();
+
+        LineDataSet lineDataSet1 = new LineDataSet(ys,"Sin");
+        lineDataSet1.setDrawCircles(false);
+        lineDataSet1.setCircleColor(Color.BLUE);
+        lineDataSets.add(lineDataSet1);
+
+        lineChart.setData(new LineData(lineDataSets));
+        lineChart.setVisibleXRangeMaximum(65f);
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/mx/itesm/metodosnumericos/PrimerPantalla.java
+++ app/src/main/java/mx/itesm/metodosnumericos/PrimerPantalla.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class PrimerPantalla extends AppCompatActivity {
 
@@ -59,8 +60,6 @@ public class PrimerPantalla extends AppCompatActivity {
                     startActivity(inti);
                 }
             });
-
-
 
         }
 }
